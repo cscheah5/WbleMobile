fd5c9414e24b7cb1212400cb4602a19211da0bd9
cscheah5 <cheahchoonsam5@gmail.com>
2025-03-11 17:31:51 +0800

    add react navigation

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React, { Component }  from "react";
+import {Button, View} from "react-native";
+
+const App = ({navigation}) => {
+    return(
+        <View>
+            <View style={{ margin:10 }}>
+                <Button title="Chicken" onPress={()=>{navigation.navigate('Animal', {
+                    title: 'Chicken',
+                    img: require('@/assets/images/chicken.jpg'),
+                })}} />
+            </View>
+            <View style={{ margin:10 }}>
+                <Button title="Koala" onPress={()=>{navigation.navigate('Animal', {
+                    title: 'Koala',
+                    img: require('@/assets/images/koala.jpg'),
+                })}} />
+            </View>
+        </View>
+    )
+}
+
+export default App;
