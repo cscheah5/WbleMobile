dda13923146710d90fb056691fca71d8c83e4a55
J1nnHAlt <jinhaowngaltacc@gmail.com>
2025-05-05 15:14:39 +0800

    socket connection established

--- flaskSocket/app.py
+++ flaskSocket/app.py
@@ -1,51 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, request
 from flask_socketio import SocketIO, emit
-from datetime import datetime
-import json
+from dotenv import load_dotenv
+import os
+import jwt
+
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-app.config['SECRET_KEY'] = 'secret'
-socketio = SocketIO(app)
+load_dotenv()  # Loads variables from .env into the environment
+app.config['SECRET_KEY'] = os.getenv("LARAVEL_JWT_SECRET")
 
-
-@app.route('/chat')
-def sessions():
-    return render_template('index.html')
-
-
-@socketio.on('my event2', namespace='/chat')
-def handle_my_custom_event(json_data):
-    print('received my event2: ' + str(json_data))
-
+# only connected users can receive messages in live
+connected_users={}
 
 @socketio.on('connect', namespace='/chat')
-def handle_connect_chat():
-    print('connected')
+def handle_connect():
+    jwtToken = request.args.get('userToken')
+    try:
+        # the algo can be find in laravel config/jwt.php
+        user = jwt.decode(jwtToken, app.config['SECRET_KEY'], algorithms=['HS256']) 
+        username = user.get('sub')
+        print(f"-------------------------------------------------User name: {username}")
+        connected_users[username] = request.sid
+        print(f"{username} connected with SID {request.sid}")
+    except Exception as e:
+        print(f"Connection rejected:", str(e))
+        return False # reject connection
 
 
-@socketio.on('mobile_client_connected', namespace='/chat')
-def handle_client_connected_chat(json):
-    print("mobile_client_connected, status:" + str(json['connected']))
-    return 'noticed'
-
-
-@socketio.on('web_client_connected', namespace='/chat')
-def handle_client_connected_chat(json):
-    print("web_client_connected, status:" + str(json['connected']))
-
-
-@socketio.on('message_sent', namespace='/chat')
-def handle_client_send_chat(json_data, methods=['GET', 'POST']):
-    print(json_data['message'])
-    user_name = json_data['sender']
-    message = json_data['message']
-    data = {
-        'timestamp': str(datetime.now()),
-        'sender': user_name,
-        'message': message,
-    }
-    emit('message_broadcast', json.dumps(data), broadcast=True)
 
 
 if __name__ == '__main__':
