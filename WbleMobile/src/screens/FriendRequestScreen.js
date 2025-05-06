b5cdada3f932424e25bc106c121226c1f0a77f94
J1nnHAlt <jinhaowngaltacc@gmail.com>
2025-05-06 23:37:19 +0800

    fix friend request socket error

--- WbleMobile/src/screens/FriendRequestScreen.js
+++ WbleMobile/src/screens/FriendRequestScreen.js
@@ -48,6 +48,7 @@ export default function FriendRequestScreen({route}) {
       setFriendRequests(prevState => {
         return [...prevState, friend];
       });
+      _loadFriendRequests();
     });
   }, []);
 
