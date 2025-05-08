3549f8b00b0e208e75785606b5bd839dc35931bd
cscheah5 <cheahchoonsam5@gmail.com>
2025-05-08 23:15:46 +0800

    Update config.example.js

--- WbleMobile/src/config/config.example.js
+++ WbleMobile/src/config/config.example.js
@@ -1,5 +1,4 @@
-const serverPath = 'http://10.0.2.2:8000';
-export const API_URL = `${serverPath}/api`;
-
-const socketUrl = 'http://10.0.2.2:5001';
-export const SOCKET_URL = `${socketUrl}`;
+{
+  "laravelApiUrl": "http://10.0.2.2:8000/api",
+  "socketServerUrl": "http://10.0.2.2:5001"
+}
